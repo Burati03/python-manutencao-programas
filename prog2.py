1954e0d00b8427f4e01b953fa469d32a7d5155f5
Burati03 <gabrielhenrique@colegiopaulodetarso.com.br>
2025-08-20 16:14:41 +0000

    hhg

--- prog2.py
+++ prog2.py
@@ -1,6 +1,5 @@
 n = int(input("Digite um número: "))
 i = 0
-while i < n:
-    if i <= 2:
-        print(i)
+while i <= 2: 
+    print(i)
     i += 1
--- prog3.py
+++ prog3.py
@@ -1,25 +1,25 @@
-valor = int(input("Digite o valor: "))
+valor = float(input("Digite o valor: "))
 
-notas1 = valor // 1
-valor = valor % 1
+notas100 = int(valor // 100)
+valor = valor % 100
 
-notas2 = valor // 2
-valor = valor % 2
-
-notas5 = valor // 5
-valor = valor % 5
-
-notas10 = valor // 10
-valor = valor % 10
-
-notas20 = valor // 20
-valor = valor % 20
-
-notas50 = valor // 50
+notas50 = int(valor // 50)
 valor = valor % 50
 
-notas100 = valor // 100
-valor = valor % 100
+notas20 = int(valor // 20)
+valor = valor % 20
+
+notas10 = int(valor // 10)
+valor = valor % 10
+
+notas5 = int(valor // 5)
+valor = valor % 5
+
+notas2 = int(valor // 2)
+valor = valor % 2
+
+notas1 = int(valor // 1)
+valor = round(valor % 1, 2) 
 
 print("100:", notas100)
 print("50 :", notas50)
